9b9be9293c7774d325d4a72f5c706cefecfd5d99
Rasmus Hansen <rashansen69@gmail.com>
2022-03-06 18:26:19 +0100

    Item Selection

--- src/components/TableItemList.js
+++ src/components/TableItemList.js
@@ -5,19 +5,60 @@ import useItems from "../hooks/useItems";
 import "../styles/itemlist.css";
 import Searchbar from "./Searchbar";
 
+function toggleSelection(item) {
+  return (arr) => {
+    if (!arr.includes(item._id)) {
+      arr.push(item._id);
+    } else {
+      const index = arr.indexOf(item._id);
+      if (index > -1) {
+        arr.splice(index, 1);
+      }
+    }
+    return arr;
+  };
+}
+
 export default function TableItemList({ items }) {
+  const [selectAll, setSelectAll] = useState(false);
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  function selectItem(item) {
+    setSelectedItems(toggleSelection(item));
+
+    console.log(selectedItems);
+  }
+
+  console.log("Hello");
   return (
     <div className="itemlist-container">
       <table className="itemlist-list card">
         <thead>
           <tr>
+            <th>
+              <input
+                type="checkbox"
+                checked={selectAll}
+                onChange={(e) => {
+                  setSelectAll(e.target.checked);
+                }}
+              ></input>
+            </th>
             <th>Item Name</th>
             <th>Stored In</th>
           </tr>
         </thead>
         <tbody>
           {items.map((item) => {
-            return <TableItemListItem key={item._id} item={item} />;
+            const isSelected = selectedItems.includes(item._id) || selectAll;
+            return (
+              <TableItemListItem
+                key={item._id}
+                item={item}
+                selected={isSelected}
+                setSelected={selectItem}
+              />
+            );
           })}
         </tbody>
       </table>
--- src/components/TableItemListItem.js
+++ src/components/TableItemListItem.js
@@ -3,12 +3,23 @@ import useItems from "../hooks/useItems";
 import { Link } from "react-router-dom";
 import ItemLink from "./ItemLink";
 
-export default function TableItemListItem({ item }) {
+export default function TableItemListItem({
+  item,
+  selected = false,
+  setSelected,
+}) {
   const items = useItems();
   const storedIn = items.getItemById(item.storedIn);
 
   return (
     <tr className="itemlist-item">
+      <td>
+        <input
+          type="checkbox"
+          checked={selected}
+          onChange={(e) => setSelected(item)}
+        />
+      </td>
       <td>
         <ItemLink item={item} />
       </td>
